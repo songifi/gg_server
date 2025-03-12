7d036b095d8927d1a33104ea02b9cb65f146c011
nnodim <nnodimobinna@gmail.com>
2025-03-12 23:05:02 +0100

    feat: add Swagger integration for API documentation

--- gasless-gossip/src/auth/auth.controller.ts
+++ gasless-gossip/src/auth/auth.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { AuthService } from './provider/auth.service';
+import { ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: CreateAuthDto })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
--- gasless-gossip/src/main.ts
+++ gasless-gossip/src/main.ts
@@ -9,11 +9,33 @@ import { LoggingService } from './logger/logger.service';
 import { CorrelationMiddleware } from './logger/correlation.middleware';
 import { Request, Response, NextFunction } from 'express';
 import { GlobalExceptionFilter } from './utils/global-exception-filter';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 // import { ValidationException } from './config/exceptions/config.exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(LoggingService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gasless Gossip')
+    .setDescription('The Gasless Gossip API documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT',
+    )
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   app.use((req: Request, res: Response, next: NextFunction) =>
     new RequestLoggerMiddleware(logger).use(req, res, next),
