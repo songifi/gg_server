441c8cb25e2f47ecf4357583e679e1bb97965b95
AdamsYab <adamsyabaya@gmail.com>
2025-03-22 18:48:07 +0100

    created message folder

--- gasless-gossip/src/messaging/message.module.ts
+++ gasless-gossip/src/messaging/message.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MessageRepository } from './messageRepository/message.Respository';
-import { MessageDocument } from './schemas/message.schema';
-import { MessageSchema } from './schemas/message.schema';
-import { MessageService } from './providers/message.Service';
-import { MessageController } from './message.controller';
+import { MessageController } from './controllers/message.controller';
+import { ConversationController } from './controllers/conversation.controller';
+import { MessageService } from './services/message.service';
+import { ConversationService } from './services/conversation.service';
+import { Message, MessageSchema } from './schemas/message.schema';
+import { Conversation, ConversationSchema } from './schemas/conversation.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: MessageDocument.name, schema: MessageSchema }])],
-  controllers: [MessageController],
-  providers: [MessageService, MessageRepository],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Message.name, schema: MessageSchema },
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
+  ],
+  controllers: [MessageController, ConversationController],
+  providers: [MessageService, ConversationService],
+  exports: [MessageService, ConversationService],
 })
-export class MessageModule {}
+export class MessagingModule {}
